c7f0d8c6443b95d10d6f2d89280fc8b0015d4015
Seyli95 <ilyesdridi0@gmail.com>
2025-03-12 15:58:24 +0100

    Delete Site directory

--- Site/js/animations.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let heroText = document.querySelector(".hero h1");
-    heroText.style.opacity = "0";
-    setTimeout(() => {
-        heroText.style.opacity = "1";
-        heroText.style.transition = "opacity 2s";
-    }, 500);
-});
